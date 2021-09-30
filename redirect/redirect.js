0c726abfa324f57d6490739d677762c3c393bab4
smnschmnck <85026647+smnschmnck@users.noreply.github.com>
2021-09-30 17:58:33 +0200

    Delete redirect.js

--- redirect/redirect.js
+++
@@ -1,6 +0,0 @@
-// const LINK = window.location.href;
-// const sublinks = LINK.split("/")
-// const lastSublink = sublinks[sublinks.length-1];
-// localStorage.setItem("room", lastSublink);
-// window.location.replace("/");
-alert("hji");
